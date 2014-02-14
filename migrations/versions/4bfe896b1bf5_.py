c86a2722fe883199795125695c94e6b9c5816d87
John Hogenmiller <john@yourtech.us>
2014-02-13 21:21:37 -0500

    adding migrations

---
+++ migrations/versions/4bfe896b1bf5_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 4bfe896b1bf5
+Revises: None
+Create Date: 2014-02-13 21:20:40.148946
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4bfe896b1bf5'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
